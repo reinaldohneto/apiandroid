924b5a0bc5ea4f762583547a8c8f71b192a4c12d
Reinaldo Hassel Neto <reinaldohasseln@gmail.com>
2022-05-31 13:24:50 -0300

    Liberado swagger para testes

--- Program.cs
+++ Program.cs
@@ -18,12 +18,8 @@ builder.Services.AddDbContext<AppContext>(opt =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
