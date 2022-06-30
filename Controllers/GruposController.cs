b0bad37a5f77141c06a3b1ea12e7a5386cb6dee0
Reinaldo Hassel Neto <reinaldohasseln@gmail.com>
2022-06-29 23:04:28 -0300

    Criado endpoint para inscrever em grupo

--- Controllers/GruposController.cs
+++ Controllers/GruposController.cs
@@ -1,6 +1,9 @@
-﻿using AppAndroid.Model;
+﻿using System.Security.Claims;
+using AppAndroid.Model;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppAndroid.Controllers;
 
@@ -10,16 +13,18 @@ namespace AppAndroid.Controllers;
 public class GruposController : ControllerBase
 {
     private readonly AppContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
 
 
-    public GruposController(AppContext context)
+    public GruposController(AppContext context, UserManager<ApplicationUser> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
     
     
     [HttpPost]
-    public async Task<ActionResult<Grupo?>> CriarLocalizacao(
+    public async Task<ActionResult<Grupo?>> CriarGrupo(
         GrupoInputViewModel? grupo)
     {
         var grupoDomain = new Grupo
@@ -31,6 +36,22 @@ public class GruposController : ControllerBase
         await _context.SaveChangesAsync();
         return Ok(grupoDomain);   
     }
+    
+    [HttpGet]
+    public async Task<ActionResult<ICollection<Grupo?>>> ListarGrupos()
+        => Ok(await _context.Grupos.ToListAsync());
+    
+    [HttpPost("{id:guid}")]
+    public async Task<ActionResult<Grupo?>> InscreverGrupo(Guid id)
+    {
+        var value = User.FindFirst(i => i.Type == ClaimTypes.NameIdentifier)?.Value;
 
+        var usuario = await _userManager.FindByIdAsync(value);
 
+        usuario.GrupoId = id;
+        await _userManager.UpdateAsync(usuario);
+        
+        await _context.SaveChangesAsync();
+        return Ok();   
+    }
 }
--- Model/ApplicationUser.cs
+++ Model/ApplicationUser.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace AppAndroid.Model;
 
 public class ApplicationUser : IdentityUser
 {
+    [JsonIgnore]
     public virtual ICollection<Localizacao> Localizacoes { get; set; }
+    [JsonIgnore]
     public virtual Grupo Grupo { get; set; }
 
     public Guid GrupoId { get; set; }
